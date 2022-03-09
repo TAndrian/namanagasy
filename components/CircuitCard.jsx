9ed6d13d4743996148a42fb1ba7df1aa18354541
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-03-09 12:33:56 +0300

    add dumb data to circuit list

--- components/CircuitCard.jsx
+++ components/CircuitCard.jsx
@@ -1,47 +1,57 @@
 import React from "react";
+import Link from "next/link";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { LazyLoadImage as Image } from "react-lazy-load-image-component";
 
 /*
 	Circuit card representing circuit data
 */
+const defaultDescription = "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae.";
 
-const CircuitCard = () => (
+const CircuitCard = ({
+	id = "circuit-id",
+	title = "Title",
+	image = "images/baobabs-morondava.jpg",
+	price = "???",
+	duration = "?",
+	description = defaultDescription
+}) => (
 	<article className="circuit-card o-h bg-white b-b f-c-st-st tr-200">
 		<div className="circuit-card__representation o-h w-100">
 			<div className="circuit-card__image-container w-100 h-100">
 				<Image
 					className="image w-100 h-100"
-					alt="Baobabs alley"
-					src="images/baobabs-morondava.jpg"
+					alt={ title }
+					src={ image }
 				/>
 			</div>
 			<div className="circuit-card__data w-100 h-100 f-c-st-be">
-				<a
-					className="share w-20px h-20px o-h white pd-10 mg-10 br-r f-r-ce-ce tr-200"
-					href="#0"
+				<div
+					className="share w-20px h-20px o-h white pd-10 mg-10 br-r f-r-ce-ce tr-200 p"
 					title="Share this circuit."
 				>
 					<Icon icon={ ["fas", "share"] } />
-				</a>
-				<p className="pd-10 pd-b-20 fs-90 white">
-					Starting from <span className="price pd-5 pd-l-10 pd-r-10 mg-l-5 lime br-5">280 $ / pers</span>
+				</div>
+				<p className="price-text pd-10 pd-b-20 fs-90 white">
+					Starting from <span className="price pd-5 pd-l-10 pd-r-10 mg-l-5 lime br-5">{ price } $ / pers</span>
 				</p>
 			</div>
 		</div>
 		<div className="circuit-card__about">
 			<div className="f-r-be-ce">
-				<h2 className="circuit-card__title fs-120 pd-10">Baobabs alley</h2>
-				<p className="circuit-card__duration pd-5 mg-r-10 fs-90 black">7 days</p>
+				<h2 className="circuit-card__title fs-120 pd-10">{ title }</h2>
+				<p className="circuit-card__duration pd-5 mg-r-10 fs-90 black">{ duration } days</p>
 			</div>
 			<div className="circuit-card__separator mg-l-10"></div>
 			<p className="circuit-card__text fs-90 pd-10 tx-j">
-				Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae.
+				{ description }
 			</p>
-			<p className="tx-c mg-t-10">
-				<button className="circuit-card__details pd-10 pd-l-20 pd-r-20 bg-green white br-5 p tr-200">
-					<Icon icon={ ["fas", "info-circle"] } /> Details
-				</button>
+			<p className="tx-c mg-t-20">
+				<Link href={ `/circuits/${id}` } passHref>
+					<a className="circuit-card__details pd-10 pd-l-20 pd-r-20 bg-green white br-5 p tr-200">
+						<Icon icon={ ["fas", "info-circle"] } /> Details
+					</a>
+				</Link>
 			</p>
 		</div>
 	</article>
--- components/CircuitList.jsx
+++ components/CircuitList.jsx
@@ -1,21 +1,63 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import CircuitCard from "components/CircuitCard";
 
 /*
 	List of circuits
 */
 
+const circuitList = [
+	{
+		id: uuidv4(),
+		title: "Baobabs Alley",
+		image: "images/baobabs-morondava.jpg",
+		price: 280,
+		duration: 7,
+		description: "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae."
+	},
+	{
+		id: uuidv4(),
+		title: "Sainte-Marie",
+		image: "images/sainte-marie-madagascar.jpg",
+		price: 335,
+		duration: 5,
+		description: "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae."
+	},
+	{
+		id: uuidv4(),
+		title: "Morondava pirogue",
+		image: "images/pirogue-morondava.jpg",
+		price: 250,
+		duration: 4,
+		description: "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae."
+	},
+	{
+		id: uuidv4(),
+		title: "Tuléar et Grand Sud",
+		image: "images/tuléar-madagascar.jpg",
+		price: 234,
+		duration: 4,
+		description: "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae."
+	},
+	{
+		id: uuidv4(),
+		title: "Traversée RN-7",
+		image: "images/rn7-madagascar.jpg",
+		price: 205,
+		duration: 3,
+		description: "Ut vel lorem a velit venenatis convallis mollis eu nisi. Aliquam consectetur ex sit amet ullamcorper vehicula. Nam in pretium velit. Sed volutpat dignissim scelerisque. Aenean molestie odio libero, vel condimentum risus iaculis vitae."
+	},
+];
+
+const mappedCircuits = circuitList.map(circuit => (
+	<CircuitCard key={ uuidv4() } { ...circuit} />
+))
+
 const CircuitList = () => (
 	<div className="circuit-list w-100">
 		<div className="container">
 			<div className="list pd-t-20 pd-b-20">
-				<CircuitCard />
-				<CircuitCard />
-				<CircuitCard />
-				<CircuitCard />
-				<CircuitCard />
-				<CircuitCard />
-				<CircuitCard />
+				{ mappedCircuits }
 			</div>
 		</div>
 	</div>
