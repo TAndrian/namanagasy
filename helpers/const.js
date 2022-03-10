db76025a97b3e0a4e556e31cfb211616850ccbed
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-03-10 17:45:48 +0300

    add service endpoints depending on development mode

--- helpers/const.js
+++ helpers/const.js
@@ -3,3 +3,5 @@
 */
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASEURL ? process.env.NEXT_PUBLIC_BASEURL : "";
+export const DEVELOPMENT = process.env.NODE_ENV === "development";
+export const PRODUCTION = process.env.NODE_ENV != "development";
