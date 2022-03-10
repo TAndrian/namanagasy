e88cb16b8ab36ee3ff379f98d0a63c3a02b281b2
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-03-10 15:52:25 +0300

    loading when clicking on card details

--- components/CircuitCard.jsx
+++ components/CircuitCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { LazyLoadImage as Image } from "react-lazy-load-image-component";
+import { usePage } from "hooks/usePage";
 
 /*
 	Circuit card representing circuit data
@@ -15,46 +16,56 @@ const CircuitCard = ({
 	price = "???",
 	duration = "?",
 	description = defaultDescription
-}) => (
-	<article className="circuit-card o-h bg-white b-b f-c-st-st tr-200">
-		<div className="circuit-card__representation o-h w-100">
-			<div className="circuit-card__image-container w-100 h-100">
-				<Image
-					className="image w-100 h-100"
-					alt={ title }
-					src={ image }
-				/>
-			</div>
-			<div className="circuit-card__data w-100 h-100 f-c-st-be">
-				<div
-					className="share w-20px h-20px o-h white pd-10 mg-10 br-r f-r-ce-ce tr-200 p"
-					title="Share this circuit."
-				>
-					<Icon icon={ ["fas", "share"] } />
+}) => {
+	const { load } = usePage();
+
+	return (
+		<article className="circuit-card o-h bg-white b-b f-c-st-st tr-200">
+			<div className="circuit-card__representation o-h w-100">
+				<div className="circuit-card__image-container w-100 h-100">
+					<Image
+						className="image w-100 h-100"
+						alt={ title }
+						src={ image }
+					/>
 				</div>
-				<p className="price-text pd-10 pd-b-20 fs-90 white">
-					Starting from <span className="price pd-5 pd-l-10 pd-r-10 mg-l-5 lime br-5">{ price } $ / pers</span>
+				<div className="circuit-card__data w-100 h-100 f-c-st-be">
+					<div
+						className="share w-20px h-20px o-h white pd-10 mg-10 br-r f-r-ce-ce tr-200 p"
+						title="Share this circuit."
+					>
+						<Icon icon={ ["fas", "share"] } />
+					</div>
+					<p className="price-text pd-10 pd-b-20 fs-90 white">
+						Starting from <span className="price pd-5 pd-l-10 pd-r-10 mg-l-5 lime br-5">{ price } $ / pers</span>
+					</p>
+				</div>
+			</div>
+			<div className="circuit-card__about">
+				<div className="f-r-be-ce">
+					<h2 className="circuit-card__title fs-120 pd-10">{ title }</h2>
+					<p className="circuit-card__duration pd-5 mg-r-10 fs-90 black">{ duration } days</p>
+				</div>
+				<div className="circuit-card__separator mg-l-10"></div>
+				<p className="circuit-card__text fs-90 pd-10 tx-j">
+					{ description }
+				</p>
+				<p className="tx-c mg-t-20">
+					<Link href={ `/circuits/${id}` } passHref>
+						<a
+							className="circuit-card__details pd-10 pd-l-20 pd-r-20 bg-green white br-5 p tr-200"
+							onClick={() => {
+								if(typeof load === "function")
+									load();
+							}}
+						>
+							<Icon icon={ ["fas", "info-circle"] } /> Details
+						</a>
+					</Link>
 				</p>
 			</div>
-		</div>
-		<div className="circuit-card__about">
-			<div className="f-r-be-ce">
-				<h2 className="circuit-card__title fs-120 pd-10">{ title }</h2>
-				<p className="circuit-card__duration pd-5 mg-r-10 fs-90 black">{ duration } days</p>
-			</div>
-			<div className="circuit-card__separator mg-l-10"></div>
-			<p className="circuit-card__text fs-90 pd-10 tx-j">
-				{ description }
-			</p>
-			<p className="tx-c mg-t-20">
-				<Link href={ `/circuits/${id}` } passHref>
-					<a className="circuit-card__details pd-10 pd-l-20 pd-r-20 bg-green white br-5 p tr-200">
-						<Icon icon={ ["fas", "info-circle"] } /> Details
-					</a>
-				</Link>
-			</p>
-		</div>
-	</article>
-);
+		</article>
+	);
+}
 
 export default CircuitCard;
